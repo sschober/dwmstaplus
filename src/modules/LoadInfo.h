0a2c64bc49d8bc2f8f8f879b4d932d1049b4fc73
Sven Schober <sven.schober@uni-ulm.de>
2014-06-11 10:19:18 +0200

    LoadInfo: Only show load averages; cut off other stuff

--- src/modules/LoadInfo.h
+++ src/modules/LoadInfo.h
@@ -1,6 +1,12 @@
 #pragma onve
 
+#include <regex>
 #include "FilesystemSource.h"
+
+using std::string;
+using std::regex;
+using std::regex_match;
+using std::smatch;
 
 /**
  * LoadInfo InfoSource
@@ -9,7 +15,24 @@
  *
  */
 
-struct LoadInfo : public FilesystemSource {
-    LoadInfo() : FilesystemSource("/proc/loadavg") {}
+class LoadInfo : public FilesystemSource {
+  public:
+  regex re_loadavgs_only;
+    LoadInfo() : FilesystemSource("/proc/loadavg"),
+                 re_loadavgs_only(regex("([0-9]*\\.[0-9]* [0-9]*\\.[0-9]* [0-9]*\\.[0-9]*).*"))
+  {}
+    /**
+     * We override match as we want to extract a line from the file
+     * which is not the first one.
+     */
+    string match(string ln) const {
+      smatch sm;
+      if(regex_match( ln, sm, re_loadavgs_only )){
+        if( 1 < sm.size() ){
+          return sm[1];
+        }
+      }
+      return string();
+    }
 };
 
