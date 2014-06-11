a27d41a959c567dabe7e6fab3c8d0ab081464ab8
Sven Schober <sven.schober@uni-ulm.de>
2014-06-11 18:20:11 +0200

    adhere to common interface in X11

--- src/X11.h
+++ src/X11.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <X11/Xlib.h>
 #include <string>
-class X11{
+
+using std::string;
+
+class X11 {
   Display *dpy = XOpenDisplay(NULL);
   public:
   bool isValid(){ return dpy; }
-  void set_status(std::string status_line){
+  void put(string status_line){
     XStoreName(dpy, DefaultRootWindow(dpy), status_line.c_str());
     XSync(dpy, False);
   }
