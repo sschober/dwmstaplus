ad37935d0dabb432860e59c736d1ac3e1c253224
Sven Schober <sven.schober@uni-ulm.de>
2014-06-11 18:17:36 +0200

    remove CPP cruft again

--- config.h
+++ config.h
@@ -4,22 +4,13 @@
 
 #include "BatteryInfo.h"
 #include "LoadInfo.h"
-
-// show calendar weeks
-#define TIME_INFO_SHOW_CALENDAR_WEEK 1
 #include "TimeInfo.h"
-
-// display free mem in MBs
-#define MEM_FREE_INFO_HUMAN_READABLE 1
 #include "MemFreeInfo.h"
-
-#ifndef SLEEP_TIME
-#define SLEEP_TIME 2
-#endif
+#include "KBToMBDecorator.h"
 
 // Defines the presence and order of information sources to be
 // displayed in the statusbar
-typedef Chain< MemFreeInfo,
-        Chain< BatteryInfo,
+typedef Chain< KBToMBDecorator<MemFreeInfo> ,
+        Chain< BatteryInfo<>,
         Chain< LoadInfo,
-               TimeInfo > > > InfoModules;
+               TimeInfo<true> > > > InfoModules;
